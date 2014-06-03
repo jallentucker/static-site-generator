40604fe01d049e13bcf97949e291e47f030dd3db
Carlo De Gregorio <cdegregorio@gmail.com>
2014-06-03 15:36:40 -0700

    completed createNewPage fxn and failing test

--- index.js
+++ index.js
@@ -10,13 +10,20 @@ var fs = require('fs');
  * @param {} 
  * @param {}  - .
  */
-module.exports.readAFile = function(file, cb) {
+var readAFile = module.exports.readAFile = function(file, cb) {
 	fs.readFile(file, { encoding: 'utf8' }, function(err, contents) {
 		cb(err, contents);
 	});
 };
 
-module.exports.createNewPage = function() {
+module.exports.createNewPage = function(file1, file2, file3, cb) {
 
-
+	readAFile(file1, function(err, defaultString) {
+		readAFile(file2, function(err, contentString) {
+		 	var outputString = defaultString.replace('{{ content }}', contentString);
+			fs.writeFile(file3, outputString, function(err) {
+				cb(err);
+			});
+	  });
+	});
 };
