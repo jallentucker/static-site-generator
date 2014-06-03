3b6e4afc99cb05fd38b0c85e1adfc4798fd5f1e1
Carlo De Gregorio <cdegregorio@gmail.com>
2014-06-03 11:17:54 -0700

    Lots of comments.

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ module.exports = {};
 var fs = require('fs');
 
 module.exports.readAFile = function(file, cb) {
+	console.log('\treadAFile: entry point');
 
-	// cb(undefined, 'My name is Joe.');
-
+	console.log('\treadAFile: starting fs.readFile');
 	fs.readFile(file, { encoding: 'utf8' }, function(err, contents) {
+		console.log('\treadAFile: fs.readFile callback executed');
+		console.log('\treadAFile: about to call cb');
 		cb(err, contents);
-		console.log(contents);
+		console.log('\treadAFile: cb call is complete');
 	});
-	
-	
-
+	console.log('\treadAFile: running code after call to fs.readFile');
+	console.log('\treadAFile: exiting function');
 };
--- test/tests.js
+++ test/tests.js
@@ -7,10 +7,15 @@ describe('readAFile()', function(done) {
 	it('returns the contents of a file', function(done) {
 		var siteFile = path.join('./testFile.txt');
 
+		console.log('\ntest: starting');
+		console.log('test: starting lib.readAFile');
 		lib.readAFile(siteFile, function (err, processedString) {
+			console.log('test: lib.readAFile callback called');
 			expect(processedString).to.eql('My name is Joe.');
 			done();
 		});
+		console.log('test: code after lib.readAFile');
+		console.log('test: exiting function');
 
 	// 	processAFile(siteFile, function(err, processed) {
  //      fs.readFile(expected, { encoding: 'utf8'}, function(err, contents) {
